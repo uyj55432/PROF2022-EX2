3c2ceeba70ea786f038a4da3c32685583cf77c05
uyj55432 <uyj55432@gmail.com>
2026-01-15 17:57:18 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -2,17 +2,27 @@ package es.upm.grise.prof2022.ex2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
-
+	
 	@Test
-	public void smokeTest() throws Exception {
+	public void testCaminoCatch() throws Exception{
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("key", Language.Italian));
 	}
 
+	@Test
+	public void testCaminoPrimerIf() throws Exception{
+		LanguageManipulation lm = new LanguageManipulation();
+		assertThrows(NonExistingKeyException.class, () -> lm.getText("key", Language.English));
+	}
+
+	@Test
+	public void testCaminoSegundoIf() throws Exception{
+		LanguageManipulation lm = new LanguageManipulation();
+		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("key", Language.Spanish));
+	}
 }
